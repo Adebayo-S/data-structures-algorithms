60720a65d01391ef88282059402223ff7973770c
Adebayo Samuel <eladebayoor@gmail.com>
2022-06-30 22:29:18 +0100

    [feat]: linear_probe.c added

---
+++ Hash-Tables/linear_probe.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#define SIZE 10
+
+int hash(int key)
+{
+	return key % SIZE;
+}
+
+int probe(int hashtable[], int key)
+{
+	int index = hash(key);
+	int i = 0;
+
+	while (hashtable[(index + i) % SIZE] != 0)
+		i++;
+
+	return (index + i) % SIZE;
+}
+
+void insert(int hashtable[], int key)
+{
+	int index = hash(key);
+
+	if (hashtable[index] != 0)
+		index = probe(hashtable, key);
+
+	hashtable[index] = key;
+}
+
+int Search(int hashtable[], int key)
+{
+	int index = hash(key);
+	int i = 0;
+
+	while(hashtable[(index+i) % SIZE] != key)
+		i++;
+
+	return (index + i) % SIZE;
+}
+
+int main()
+{
+	int hashtable[10] = {0};
+
+	insert(hashtable, 12);
+	insert(hashtable, 25);
+	insert(hashtable, 35);
+	insert(hashtable, 26);
+
+	printf("\nKey found at %d\n", Search(hashtable, 35));
+	return (0);
+}
