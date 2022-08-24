c9993556872f45bda562628a1286a91d2857695c
Adebayo Samuel <eladebayoor@gmail.com>
2022-08-24 21:24:54 +0100

    [feat]: depth first search algo created

---
+++ Binary-Trees/Javascript/depthFirstSearch.js
@@ -0,0 +1,24 @@
+
+
+const depthFirstValues = (root) => {
+    if (!root) return [];
+
+    const result = [];
+    const stack = [ root ];
+
+    while (stack.length > 0) {
+        const current = stack.pop();
+        result.push(current.val);
+
+        if (current.right) stack.push(current.right);
+        if (current.left) stack.push(current.left);
+    }
+    return result;
+};
+
+const depthFirstRec = (root) => {
+    if (root == null) return [];
+    const leftValues = depthFirstRec(root.left); // [b, d, e]
+    const rightValues = depthFirstRec(root.right); // [c, f]
+    return [ root.val, ...leftValues, ...rightValues ];
+}
