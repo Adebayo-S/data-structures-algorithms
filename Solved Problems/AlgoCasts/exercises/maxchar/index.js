6cfb07302b7daee0d2cafa5ee15f34596802737a
Adebayo Samuel <eladebayoor@gmail.com>
2022-11-23 06:18:27 +0100

    [feat]: maxchar js added

--- Solved Problems/AlgoCasts/exercises/maxchar/index.js
+++ Solved Problems/AlgoCasts/exercises/maxchar/index.js
@@ -5,6 +5,49 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let counter = {};
 
+    for (let char of str) {
+        if (!counter[char]) {
+            counter[char] = 1;
+        } else {
+            counter[char] += 1;
+        }
+    }
+
+    let arr = Object.values(counter);
+    let max = Math.max(...arr);
+
+    for (let key of Object.keys(counter)) {
+        if (counter[key] === max) {
+            return key;
+        }
+    }
+}
+
+console.log(maxChar('aaanhd'));
 module.exports = maxChar;
+
+function maxChar(str) {
+    let counter = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (let char of str) {
+        if (counter[char]) {
+            counter[char]++;
+        } else {
+            counter[char] = 1;
+        }
+    }
+
+    for (let key in counter) {
+        if (counter[key] > max) {
+            max = counter[key];
+            maxChar = key;
+        }
+    }
+
+    return maxChar;
+}
