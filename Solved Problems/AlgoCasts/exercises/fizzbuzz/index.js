c217ebecbdc34197e51d9f3d88c04b31f8b18095
Adebayo Samuel <eladebayoor@gmail.com>
2022-11-23 06:36:19 +0100

    [feat]: fizzbuzz js added

--- Solved Problems/AlgoCasts/exercises/fizzbuzz/index.js
+++ Solved Problems/AlgoCasts/exercises/fizzbuzz/index.js
@@ -12,6 +12,18 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for (let num = 1; num <= n; num++) {
+        if (num % 3 === 0 && num % 5 === 0) {
+            console.log("fizzbuzz");
+        } else if (num % 5 === 0) {
+            console.log("buzz");
+        } else if (num % 3 === 0) {
+            console.log("fizz");
+        } else {
+            console.log(num);
+        }
+    }
+}
 
 module.exports = fizzBuzz;
