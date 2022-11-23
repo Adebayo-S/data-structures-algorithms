3f7b57910a2722da0a5c00f2dbc40629e9fd475d
Adebayo Samuel <eladebayoor@gmail.com>
2022-11-23 08:26:50 +0100

    [feat]: anagrams js added

--- Solved Problems/AlgoCasts/exercises/anagrams/index.js
+++ Solved Problems/AlgoCasts/exercises/anagrams/index.js
@@ -8,6 +8,78 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    let punctuation = ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+
+    let cleanA = stringA.split('').filter(char => {
+        return punctuation.indexOf(char) === -1;
+    }).join('').toLowerCase();
+
+    let cleanB = stringB.split('').filter(char => {
+        return punctuation.indexOf(char) === -1;
+    }).join('').toLowerCase();
+
+    if (cleanA.length !== cleanB.length) return false;
+
+    let counterA = {};
+    let counterB = {};
+
+    for (let char of cleanA) {
+        if (!counterA[char]) {
+            counterA[char] = 1;
+        } else {
+            counterA[char]++;
+        }
+    }
+
+    for (let char of cleanB) {
+        if (!counterB[char]) {
+            counterB[char] = 1;
+        } else {
+            counterB[char]++;
+        }
+    }
+
+    for (let key in counterA) {
+        if (counterA[key] !== counterB[key]) return false;
+    }
+
+    return true;
+}
 
 module.exports = anagrams;
+
+function buildCharMap(str) {
+    const charMap = {};
+
+    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    return charMap;
+}
+
+function anagrams(stringA, stringB) {
+    const aCharMap = buildCharMap(stringA);
+    const bCharMap = buildCharMap(stringB);
+
+    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+        return false;
+    }
+
+    for (let char in aCharMap) {
+        if (aCharMap[char] !== bCharMap[char]) return false;
+    }
+
+    return true;
+}
+
+function cleanString(str) {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
+
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB);
+}
+
+
