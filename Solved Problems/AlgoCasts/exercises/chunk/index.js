f4c1649566001cbd6e679cd93cabadd38e3bb753
Adebayo Samuel <eladebayoor@gmail.com>
2022-11-23 07:26:11 +0100

    [feat]: chunk js added

--- Solved Problems/AlgoCasts/exercises/chunk/index.js
+++ Solved Problems/AlgoCasts/exercises/chunk/index.js
@@ -8,6 +8,48 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let newArray = [];
+    let temp = [];
+    for (let num in array) {
+        temp.push(array[num]);
+        if ((parseInt(num)+ 1) % size === 0) {
+            newArray.push(temp);
+            temp = [];
+        }
+    }
+    if (array.length % size !== 0) {
+        newArray.push(temp);
+    }
+    return newArray;
+}
 
 module.exports = chunk;
+
+function chunk(array, size) {
+    const chunked = [];
+
+    for (let element of array) {
+        const last = chunked[chunked.length - 1];
+
+        if (!last || last.length === size) {
+            chunked.push([element]);
+        } else {
+            last.push(element);
+        }
+    }
+
+    return chunked;
+}
+
+function chunk(array, size) {
+    const chunked = [];
+    let index = 0;
+
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
+    }
+
+    return chunked;
+}
