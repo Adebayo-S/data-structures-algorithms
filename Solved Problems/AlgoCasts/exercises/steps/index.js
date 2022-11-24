3985679ac438bb0d5d3a42cac9447576650ed65e
Adebayo Samuel <eladebayoor@gmail.com>
2022-11-24 02:05:50 +0100

    [feat]: steps js added

--- Solved Problems/AlgoCasts/exercises/steps/index.js
+++ Solved Problems/AlgoCasts/exercises/steps/index.js
@@ -17,6 +17,34 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    for (let i = 1; i <= n; i++) {
+        let row = '';
+        for (let j = 1; j <= n; j++) {
+            if (i >= j) row += '#';
+            else row += ' ';
+        }
+        console.log(row);
+    }
+}
 
 module.exports = steps;
+
+function steps(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
+    }
+
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+
+    steps(n, row, stair);
+}
