65063d061c939d25a896b23d900df6a032e01c38
Adebayo Samuel <eladebayoor@gmail.com>
2022-11-24 03:52:17 +0100

    [feat]: vowels js added

--- Solved Problems/AlgoCasts/exercises/vowels/index.js
+++ Solved Problems/AlgoCasts/exercises/vowels/index.js
@@ -7,6 +7,17 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+
+    for (char of vowels) {
+        if (str.toLowerCase().includes(char)) {
+            count++;
+        }
+    }
+
+    return count;
+}
 
 module.exports = vowels;
